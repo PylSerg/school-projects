b24475f7bb118ba6d1b7758ca2d6790a69abc77d
Serhii <pylypenko.sn@gmail.com>
2024-09-18 16:48:03 +0300

    adds timer

--- 24-25/8-D/keys-codes/keys-codes.py
+++ 24-25/8-D/keys-codes/keys-codes.py
@@ -1,9 +1,22 @@
 import keyboard
+import time
+
+last_time = 0
 
 print(f"\n Key  ==>  Code \n")
 
 def on_key_event(event):
-    print(f"{event.name}  ==>  {event.scan_code} \n")
+    global last_time
+
+    current_time = time.time()
+
+    if current_time - last_time >= 1:
+        print(f"{event.name}  ==>  {event.scan_code} \n")
+
+        last_time = current_time
+    #if
+#def
+
 
 keyboard.hook(on_key_event)
 keyboard.wait()
