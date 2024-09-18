01ab89f11d1d62d6b8ce3809aa7a8d3df9140f8b
Serhii <pylypenko.sn@gmail.com>
2024-09-18 16:39:48 +0300

    changes log

--- 24-25/8-D/keys-codes/keys-codes.py
+++ 24-25/8-D/keys-codes/keys-codes.py
@@ -3,7 +3,7 @@ import keyboard
 print(f"\n Key  ==>  Code \n")
 
 def on_key_event(event):
-    print(f"{event.name}  ==>  {event.scan_code}")
+    print(f"{event.name}  ==>  {event.scan_code} \n")
 
 keyboard.hook(on_key_event)
 keyboard.wait()
