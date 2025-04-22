0b952f01fffaf490654eac7cadad78f08f9e2d4d
Serhii <pylypenko.sn@gmail.com>
2025-04-22 20:00:52 +0300

    Додано файл з налаштуванням черепашки

---
+++ 24-25/7-E/черепаха-заняття-01.py
@@ -0,0 +1,24 @@
+# from turtle import * - імпорт бібліотеки turtle
+
+# bgcolor() - задає колір фону
+# setup() - задає розміри вікна
+# shape("turtle") - задає форму черепахи
+# speed() - задає швидкість черепахи (1-10)
+# width() - задає товщину лінії
+# color() - задає колір лінії
+# done() - закінчує виконання програми
+
+from turtle import *
+
+bgcolor("lightblue")
+setup(width=800, height=800)
+
+shape("turtle")
+speed(5)
+
+width(10)
+color("green")
+
+forward(300)
+
+done()
