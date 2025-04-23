e3b46755e156a76dc946e49d4d2c0a411ee4f3b6
Serhii <pylypenko.sn@gmail.com>
2025-04-23 18:14:36 +0300

    Додано обрахунок площі та периметру

--- 24-25/8-D/tkinter-обчислення-прямокутника.py
+++ 24-25/8-D/tkinter-обчислення-прямокутника.py
@@ -1,4 +1,26 @@
+# justify - вирівнювання тексту по ширині ("left", "right", "center")
+# wraplength - максимальна ширина тексту в пікселях, після якої текст буде перенесено на новий рядок
+
 from tkinter import Tk, Label, Button, Entry
+
+# Функція для обчислення площі та периметра прямокутника
+def calc():
+    # Отримання значень сторін A та B з полів вводу
+    # Перетворення значень у тип float
+    a = float(side_a_entry.get())
+    b = float(side_b_entry.get())
+    
+    # обчислення площі та периметра
+    area = a * b
+    perimeter = 2 * (a + b)
+    
+    # Виведення результатів у текстові написи
+    area_label["text"] = f"Площа прямокутника зі сторонами {a} од. та {b} од. дорівнює {area} од.²"
+    perimeter_label["text"] = f"Периметр прямокутника зі сторонами {a} од. та {b} од. дорівнює {perimeter} од."
+    
+    # Очищення полів вводу
+    side_a_entry.delete(0, 'end')
+    side_b_entry.delete(0, 'end')
 
 root = Tk()
 root.title("Обчислення прямокутника")
@@ -13,12 +35,12 @@ side_b_label = Label(root, text="Сторона B:")
 side_b_entry = Entry(root)
 
 # Створення кнопки для обчислення
-calc_button = Button(root, text="Обчислити", width=15, height=2)
+calc_button = Button(root, text="Обчислити", width=15, height=2, command=calc)
 
 # Створення текстового напису для результату
 result_label = Label(root, text="РЕЗУЛЬТАТ:", font=("Arial", 14, "bold"))
-area_label = Label(root)
-perimeter_label = Label(root)
+area_label = Label(root, font=("Arial", 12), justify="left", wraplength=350)
+perimeter_label = Label(root, font=("Arial", 12), justify="left", wraplength=350)
 
 
 
@@ -28,6 +50,10 @@ side_a_entry.place(x=100, y=22)
 side_b_label.place(x=20, y=50)
 side_b_entry.place(x=100, y=52)
 calc_button.place(x=250, y=25)
+
+result_label.place(x=20, y=150)
+area_label.place(x=20, y=200)
+perimeter_label.place(x=20, y=250)
 
 
 
