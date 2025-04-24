98e6be74652c6b442729c38f3dfe00552d97ab0b
Serhii <pylypenko.sn@gmail.com>
2025-04-24 20:40:33 +0300

    Додано квадрат

--- 24-25/7-E/черепаха-заняття-02.py
+++ 24-25/7-E/черепаха-заняття-02.py
@@ -6,58 +6,29 @@ speed(5)
 
 width(10)
 
+
+# Малюємо квадрат
 up()
-goto(-300, 0)
-
-down()
-color("red")
-circle(100)
-
-up()
-goto(-150, 0)
-
+goto(-300, 200)
 down()
 color("blue")
-circle(100)
+fillcolor("yellow")
 
-up()
-goto(0, 0)
+begin_fill()
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+end_fill()
 
-down()
-color("green")
-circle(100)
 
-up()
-goto(150, 0)
-
-down()
-color("yellow")
-circle(100)
-
-up()
-goto(-225, -150)
-
-down()
-color("purple")
-circle(100)
-
-up()
-goto(-75, -150)
-
-down()
-color("orange")
-circle(100)
-
-up()
-goto(75, -150)
-
-down()
-color("pink")
-circle(100)
-
+# Малюємо рівносторонній трикутник
 up()
 goto(300, 200)
-
 down()
 color("black", "tomato")
 
@@ -69,4 +40,51 @@ left(120)
 forward(100)
 end_fill()
 
+
+# Малюємо олімпійські кільця
+up()
+goto(-300, 0)
+down()
+color("red")
+circle(100)
+
+up()
+goto(-150, 0)
+down()
+color("blue")
+circle(100)
+
+up()
+goto(0, 0)
+down()
+color("green")
+circle(100)
+
+up()
+goto(150, 0)
+down()
+color("yellow")
+circle(100)
+
+up()
+goto(-225, -150)
+down()
+color("purple")
+circle(100)
+
+up()
+goto(-75, -150)
+down()
+color("orange")
+circle(100)
+
+up()
+goto(75, -150)
+down()
+color("pink")
+circle(100)
+
+
+# Ховаємо черепашку та завершуємо виконання програми
+hideturtle()
 done()
