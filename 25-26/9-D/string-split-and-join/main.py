34c0520d5780e907280ace2af898c5e77147e0e3
Serhii <39593488+PylSerg@users.noreply.github.com>
2026-04-17 17:39:52 +0300

    feat: add script to demonstrate string splitting and joining with case transformations

---
+++ 25-26/9-D/string-split-and-join/main.py
@@ -0,0 +1,9 @@
+text = "Setting up developement environment PyCharm python-gtk windows"
+
+text_list = text.split()
+
+new_text_1 = "-".join(text_list).lower()
+new_text_2 = "_".join(text_list).upper()
+
+print(f"\n{new_text_1}")
+print(f"\n{new_text_2}")
