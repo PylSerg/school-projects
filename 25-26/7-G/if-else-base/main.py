d96b378ac1e39394bbee65345539aac76cef6da7
Serhii <39593488+PylSerg@users.noreply.github.com>
2026-04-23 19:19:29 +0300

    feat: initialize main.py with base if-else logic structure

---
+++ 25-26/7-G/if-else-base/main.py
@@ -0,0 +1,9 @@
+a = int(input("Введіть перше число: "))
+b = int(input("Введіть друге число: "))
+
+if a > b:
+    print(f"\n a = {a}")
+elif b > a:
+    print(f"\n b = {b}")
+else:
+    print(f"\n a = b = {a}")
