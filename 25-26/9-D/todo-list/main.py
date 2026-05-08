81b60bccebdd61af58d96d69938a8d5d0bed75bf
PylSerg <pylypenko.sn@gmail.com>
2026-05-08 17:40:29 +0300

    feat: implement task addition functionality and create GUI for todo list application

--- 25-26/9-D/todo-list/main.py
+++ 25-26/9-D/todo-list/main.py
@@ -13,3 +13,32 @@ try:
 except:
     with open(TODO_LIST_FILE, "w") as file:
         todos = json.dump(file)
+
+
+
+def add_new_task():
+    global todos
+
+    todos.insert(0, input_entry.get())
+
+    with open(TODO_LIST_FILE, "w", encoding="utf-8") as file:
+        todos = json.dump(file)
+
+    input_entry.delete(0, "end")
+
+
+
+root = tk.Tk()
+root.title("Список завдань")
+root.geometry("600x650")
+root.resizable(False, False)
+
+input_label = tk.Label(root, text="Введіть нове завдання:", font="Arial 14 bold")
+input_entry = tk.Entry(root, font="Arial 14 bold", width=50)
+add_task_button = tk.Button(root, text="Додати завдання", command=add_new_task)
+
+input_label.place(x=20, y=20)
+input_entry.place(x=20, y=50)
+add_task_button.place(relx=0.5, y=100, anchor="center")
+
+root.mainloop()
