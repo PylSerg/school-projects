e02b9863ad7ee03fd73cbcd3657325266d75c15a
PylSerg <pylypenko.sn@gmail.com>
2026-05-14 11:34:36 +0300

    feat: implement dynamic todo list rendering by adding update_todos_list function and UI frame

--- 25-26/9-D/todo-list/main.py
+++ 25-26/9-D/todo-list/main.py
@@ -15,14 +15,31 @@ except:
         json.dump(todos, file)
 
 
+def update_todos_list():
+    global todos_frame
+
+    todos_frame.destroy()
+
+    todos_frame = tk.Frame(root, width=500, height=500)
+    todos_frame.place(x=20, y=180)
+
+    for indx, todo in enumerate(todos, start=1):
+        position_y = indx * 40
+
+        task_label = tk.Label(todos_frame, text=f"{indx}. {todo}", font="Arial 14", justify="left")
+        task_label.place(x=20, y=position_y)
+
+
 
 def add_new_task():
     todos.insert(0, input_entry.get())
 
-    with open(TODO_LIST_FILE, "w", encoding="utf-8") as file:
+    with open(TODO_LIST_FILE, "w") as file:
         json.dump(todos, file)
 
     input_entry.delete(0, "end")
+    
+    update_todos_list()
 
 
 
@@ -35,8 +52,15 @@ input_label = tk.Label(root, text="Введіть нове завдання:", f
 input_entry = tk.Entry(root, font="Arial 14 bold", width=50)
 add_task_button = tk.Button(root, text="Додати завдання", command=add_new_task)
 
+todos_label = tk.Label(root, text="Список завдань", font="Arial 14 bold")
+todos_frame = tk.Frame(root)
+
+
 input_label.place(x=20, y=20)
 input_entry.place(x=20, y=50)
 add_task_button.place(relx=0.5, y=100, anchor="center")
+todos_label.place(x=20, y=150)
+
+update_todos_list()
 
 root.mainloop()
